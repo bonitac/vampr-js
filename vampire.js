6d3ba44336902cc4eca3517ef751f695dfc81379
Bonita Choi <bonitac93@gmail.com>
2019-04-04 20:04:18 +0000

    completed all functions except stretch

--- vampire.js
+++ vampire.js
@@ -10,22 +10,30 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVampires = 0;
+    let currentVampire = this;
 
+    while(currentVampire.creator){
+      currentVampire = currentVampire.creator;
+      numberOfVampires++;
+    }
+    return numberOfVampires;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
   }
 
   /** Stretch **/
@@ -36,7 +44,19 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
-
+    let vampire1 = this;
+    while (vampire1.creator && vampire.creator){
+      if (vampire1.creator === vampire.creator){
+        return vampire1.creator;
+      } else if (vampire1 === vampire.creator){
+        return vampire1;
+      } else if (vampire1.creator === vampire){
+        return vampire;
+      }
+      vampire1=vampire1.creator;
+      vampire = vampire.creator;
+    }
+    return vampire1;
   }
 }
 
